a35671b16e4c3efbbc9fc9178e0b2d9fe1fc43dd
Tate <tate.hill@students.fhu.edu>
2026-09-20 01:11:06 -0500

    MergeArrays passes unittests

--- MergeArrays/Program.cs
+++ MergeArrays/Program.cs
@@ -4,14 +4,57 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        string[] names1 = {"andy", "bart", "zack"};
+        string[] names2 = {"ben", "wisdom", "xavier"};
 
+        var sortedNames = MergeSortedArrays<string>(names1, names2);
+
+        System.Console.WriteLine(sortedNames[0]);
     }
 
     // TODO 
     public static int[] MergeSortedArrays(int[] array1, int[] array2)
     {
-        return null;
+        int[] result = new int[array1.Length + array2.Length];
+        int index1=0;
+        int index2=0;
+        int indexResult=0;
+
+        //loop
+        while (index1 < array1.Length && index2 < array2.Length)
+        {
+            if(array1[index1].CompareTo(array2[index2]) <= 0)
+            {
+                result[indexResult] = array1[index1];
+                index1++;
+            }
+            else
+            {
+                result[indexResult] = array2[index2];
+                index2++;
+            }
+            indexResult++;
+        }
+
+        //end loop
+
+        while (index1 < array1.Length)
+        {
+            result[indexResult] = array1[index1];
+            index1++;
+            indexResult++;
+        }
+
+        while (index2 < array2.Length)
+        {
+            result[indexResult] = array2[index2];
+            index2++;
+            indexResult++;
+        }
+
+        return result;     
     }
+
 
     // TODO 
     private static bool IsSorted(int[] array)
@@ -22,8 +65,45 @@ public class Program
     // TODO
     public static T[] MergeSortedArrays<T>(T[] array1, T[] array2) where T:IComparable<T>
     {
+        T[] result = new T[array1.Length + array2.Length];
 
-        return default;     
+        int index1=0;
+        int index2=0;
+        int indexResult=0;
+
+        //loop
+        while (index1 < array1.Length && index2 < array2.Length)
+        {
+            if(array1[index1].CompareTo(array2[index2]) <= 0)
+            {
+                result[indexResult] = array1[index1];
+                index1++;
+            }
+            else
+            {
+                result[indexResult] = array2[index2];
+                index2++;
+            }
+            indexResult++;
+        }
+
+        //end loop
+
+        while (index1 < array1.Length)
+        {
+            result[indexResult] = array1[index1];
+            index1++;
+            indexResult++;
+        }
+
+        while (index2 < array2.Length)
+        {
+            result[indexResult] = array2[index2];
+            index2++;
+            indexResult++;
+        }
+
+        return result;     
     }
-}
 
+}
